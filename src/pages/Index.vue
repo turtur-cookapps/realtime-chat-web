af69f48bce91c180ba5c39fef4d12a5a706ebfce
turtur <yjko@cookapps.com>
2018-09-06 15:17:02 +0900

    채팅내역 하나만 데이터와 연결해보기

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,15 +2,10 @@
   <q-page>
     <div style="width: 500px; max-width: 90vw;">
       <q-chat-message
-        name="야임마"
-        avatar="statics/boy-avatar.png"
-        :text="['뭐']"
+        :name=chat.name
+        :avatar=chat.avatar
+        :text=chat.text
         sent
-      />
-      <q-chat-message
-        name="ㅎㅎㅎㅎ"
-        avatar="statics/linux-avatar.png"
-        :text="['hey, if you type in your pw', 'it will show as stars']"
       />
     </div>
   </q-page>
@@ -24,7 +19,7 @@ export default {
   name: 'PageIndex',
   data () {
     return {
-      chats: [{name: 'aaa', avatar: 'bbb'}]
+      chat: { name: 'aaa', avatar: 'aaa', text: ['agsgzgdgzdgsgfsfzsdff'] }
     }
   }
 }
