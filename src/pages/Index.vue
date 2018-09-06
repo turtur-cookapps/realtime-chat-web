0ffe641c0eb7d74690c19af541810e0a825e82dd
turtur <yjko@cookapps.com>
2018-09-06 13:30:08 +0900

    전체 사이즈 조절

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,22 +1,24 @@
 <template>
-  <q-page>
-    <q-chat-message
-      label='Sunday, 19th'
-    />
-    <q-chat-message
-      name="me"
-      avatar="statics/boy-avatar.png"
-      :text="['hunter2']"
-      stamp="4 minutes ago"
-      sent
-    />
-    <q-chat-message
-      name="Jane"
-      avatar="statics/linux-avatar.png"
-      :text="['hey, if you type in your pw', 'it will show as stars']"
-      stamp="7 minutes ago"
-    />
-  </q-page>
+  <div style="width: 500px; max-width: 90vw;">
+    <q-page>
+      <q-chat-message
+        label='Sunday, 19th'
+      />
+      <q-chat-message
+        name="me"
+        avatar="statics/boy-avatar.png"
+        :text="['hunter2']"
+        stamp="4 minutes ago"
+        sent
+      />
+      <q-chat-message
+        name="Jane"
+        avatar="statics/linux-avatar.png"
+        :text="['hey, if you type in your pw', 'it will show as stars']"
+        stamp="7 minutes ago"
+      />
+    </q-page>
+  </div>
 </template>
 
 <style>
