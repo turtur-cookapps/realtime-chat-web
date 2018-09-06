633092130d62c9b009b2a025cacba7b9b0c7a788
turtur <yjko@cookapps.com>
2018-09-06 13:28:18 +0900

    일단 홈페이지 예제 복붙

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,21 @@
 <template>
-  <q-page class="flex flex-center">
-
+  <q-page>
+    <q-chat-message
+      label='Sunday, 19th'
+    />
+    <q-chat-message
+      name="me"
+      avatar="statics/boy-avatar.png"
+      :text="['hunter2']"
+      stamp="4 minutes ago"
+      sent
+    />
+    <q-chat-message
+      name="Jane"
+      avatar="statics/linux-avatar.png"
+      :text="['hey, if you type in your pw', 'it will show as stars']"
+      stamp="7 minutes ago"
+    />
   </q-page>
 </template>
 
